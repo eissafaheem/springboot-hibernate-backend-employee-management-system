0291d04cbccfeb8414f38f7e17397ab5fc97c5b9
Eissa Faheem <eissa.faheem@ziroh.com>
2023-03-22 13:40:03 +0530

    added docker

--- src/main/java/com/EMS/employee_management_system/EmployeeManagementSystemApplication.java
+++ src/main/java/com/EMS/employee_management_system/EmployeeManagementSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan("com.EMS.employee_management_system.models.classes")
 public class EmployeeManagementSystemApplication {
-
+	public String PORT =System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
